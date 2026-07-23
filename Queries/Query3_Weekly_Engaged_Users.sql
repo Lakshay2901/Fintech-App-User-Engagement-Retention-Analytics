13205cca4ede74d1d43e96b611e17579ac6c41da
Lakshay Bhatia <69351489+Lakshay2901@users.noreply.github.com>
2026-07-23 23:32:03 +0530

    Update Query3_Weekly_Engaged_Users.sql

--- Queries/Query3_Weekly_Engaged_Users.sql
+++ Queries/Query3_Weekly_Engaged_Users.sql
@@ -5,7 +5,7 @@ WITH daily_activity AS (
     SELECT DISTINCT
         user_id,
         txn_date,
-        YEARWEEK(txn_date, 3) AS txn_week 
+        YEARWEEK(txn_date, 3) AS txn_week  
     FROM transactions
     WHERE status = 'Success'
 ),
